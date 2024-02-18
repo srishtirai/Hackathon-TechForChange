dabadb765039d5b3972e1662570f6184f78e9665
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-02-18 06:04:21 -0500

    Achivements

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -3,8 +3,8 @@ import "./Dashboard.css";
 import { getUserDetails } from "../api";
 import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
-import { Progress } from 'semantic-ui-react';
-import { getXPLevel} from '../constants';
+import { Progress, Image, Label, LabelDetail, Icon } from 'semantic-ui-react';
+
 
 export const Dashboard = () => {
   const [userDetails, setUserDetails] = useState({});
@@ -24,23 +24,28 @@ export const Dashboard = () => {
         <div className="body">
           <div className="content">
             <div className="overlap-group-2">
-              <img
-                className="DALLE"
-                alt="Dalle"
-                src={userDetails.profile_url ? userDetails.profile_url : "https://c.animaapp.com/ZxZGK3Jl/img/dall-e-2024-02-17-17-55-21---redesign-the-set-of-circular-icons-@2x.png"}
-              />
+              <Image src={userDetails.profile_url ? userDetails.profile_url : "https://c.animaapp.com/ZxZGK3Jl/img/dall-e-2024-02-17-17-55-21---redesign-the-set-of-circular-icons-@2x.png"}  circular size="tiny"/>
             </div>
             <div className="text-wrapper-6">{userDetails.name}</div>
             <div className="text-wrapper-7">Rewards</div>
-            <Progress percent={60} active>
-              Active
-            </Progress>
+            <div className="progress">
+              <div className="progress-display">
+                <Progress percent={((userDetails["credits"]/1000)*100)} active color="green">
+                {userDetails["credits"]}/1000 XP
+                <br/>
+                <br/>
+                <Label as='a' color="brown">
+                  Level
+                  <LabelDetail>1</LabelDetail>
+                </Label>
+                </Progress>
+              </div>
+            </div>
             <div className="overlap">
               <div className="text-wrapper-10">Achievements</div>
               <div className="rectangle-3" />
             </div>
             <div className="text-wrapper-9">Events</div>
-            <div className="text-wrapper-12">20/1000 XP</div>
             <img className="line" alt="Line" src="https://c.animaapp.com/ZxZGK3Jl/img/line-1.svg" />
             <div className="rectangle-4" />
           </div>
